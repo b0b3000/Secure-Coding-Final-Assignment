163552bd6a1d4c180364e493261840c0f68d758a
Bob Beashel <b0b3000@users.noreply.github.com>
2023-10-10 06:49:50 +0000

    njjj

--- p_and_p.c
+++ p_and_p.c
@@ -10,12 +10,19 @@
 #include <pwd.h>
 #include <sys/stat.h>
 
-#define MAXINTDIGITS 21
-
 //CC=gcc CFLAGS="-std=c11 -pedantic -Wall -Wextra -Wshadow -Wconversion -O" make p_and_p.c p_and_p.o
 //make p_and_p
 // ./p_and_p
 
+/** Validates and serialises a given array of ItemDetails structs to a file specified by the caller. 
+* The written file will contain a header, with the number of ItemDetails struct serialised,
+* followed by one or more blocks, containing information about each struct.
+* The function will return 0 upon success, and 1 otherwise.
+ * \param arr Array of ItemDetails structs
+ * \param nmemb Number of member ItemDetails structs in array 'arr'
+ * \param fd File Descriptor of file to be serialise to
+ * \return 0 if succesfully serialised, 1 if not succesfully serialised
+ */
 int saveItemDetails(const struct ItemDetails* arr, size_t nmemb, int fd) {
 
   if(fd<=2){
@@ -23,7 +30,6 @@ int saveItemDetails(const struct ItemDetails* arr, size_t nmemb, int fd) {
     return 1;
   }
 
-  //This might not be necessary
   int fdDup = dup(fd);
 
   if(fdDup<=2){
@@ -31,13 +37,13 @@ int saveItemDetails(const struct ItemDetails* arr, size_t nmemb, int fd) {
     return 1;
   }
 
-  //check dup success
-
   FILE* fp = fdopen(fdDup, "w");
+  if (!fp){
+    printf("Error opening file");
+    fflush(fp);
+    return 1;
+  }
   fflush(fp);
-  //check success of fdopen
-
-
 
   //Header
   uint64_t numItems = nmemb;
@@ -64,55 +70,64 @@ int saveItemDetails(const struct ItemDetails* arr, size_t nmemb, int fd) {
     }
   }
 
-  fflush(fp);
-  fclose(fp);
+  if (fclose(fp) != 0){
+    printf("File closing error");
+    return 1;
+  }
   return 0;
 }
 
-
-
-
-
+/** If succesful, this function will deserialise details of items from a file into a pointer 
+* array of ItemDetails structs. The header of the file contains the number of itemDetails 
+* elements to be deserialised, and is written into 'nmemb'
+* This number of ItemDetails elements is deserialised from the file specified by the inputted 
+* file descriptor, each validated and written into 'ptr'.
+* The memory of ptr is dynamically allocated, and should be freed by the caller
+* The function returns 0 on succesful deserialisation, and 1 otherwise
+ *
+ * \param ptr Pointer to ItemDetails struct pointer array to be deserialised into
+ * \param numItems Pointer to variable to have the number of ItemDetails elements written into
+ * \param fd File Descriptor to deserialise from
+ * \return 0 if successfully deserialised, 1 otherwise
+ */
 int loadItemDetails(struct ItemDetails** ptr, size_t* nmemb, int fd) {
 
+  if(fd<=2){
+    printf("File fdDup error\n");
+    return 1;
+  }
+
   int fdDup = dup(fd);
-  //check dup success
+
   if(fdDup<=2){
     printf("File fdDup error\n");
     return 1;
   }
 
   FILE* fp = fdopen(fdDup, "r");
-  fflush(fp);
-
-  if(fd<=2){
-    printf("File fd error%i\n", fd);
-    fflush(fp);
-    fclose(fp);
+  if (!fp){
+    printf("Error opening file");
     return 1;
   }
+  fflush(fp);
 
-  //uint64_t* headerBuf = (uint64_t*)malloc(sizeof(uint64_t));
   uint64_t headerBuf;
   
   if(fread(&headerBuf, sizeof(uint64_t), 1, fp) <= 0){
     printf("ERROR reading header");
     fflush(fp);
-    //free(headerBuf);
     fclose(fp);
     return 1;
-    //FLESH OUT
   }
-
-  printf("HEADER%ld\n", headerBuf);
-  fflush(stdout);
 
   *nmemb = headerBuf;
 
-  //size_t numElValue = (size_t) atoi(headerBuf);
-  //*numEls = numElValue;
-
   struct ItemDetails *structArrayBuffer = calloc(*nmemb, sizeof(struct ItemDetails));
+
+  if (!structArrayBuffer){
+    printf("Error allocating memory\n");
+    return 1;
+  }
   
   for (size_t i = 0; i < *nmemb; i++){
 
@@ -124,15 +139,12 @@ int loadItemDetails(struct ItemDetails** ptr, size_t* nmemb, int fd) {
       free(structArrayBuffer);
       fflush(fp);
       fclose(fp);
-
-
       return 1;
-      //FLESH OUT
+
     }
 
     if(!isValidItemDetails(&structBuffer)){
       printf("Invalid ItemDetails found in file");
-      free(nmemb);
       free(structArrayBuffer);
       fflush(fp);
       fclose(fp);
@@ -144,12 +156,25 @@ int loadItemDetails(struct ItemDetails** ptr, size_t* nmemb, int fd) {
   }
 
   *ptr = structArrayBuffer;
-  fflush(fp);
-  fclose(fp);
+
+  if (fclose(fp) != 0){
+    printf("File closing error");
+    return 1;
+  }
+
   return 0;
 
 }
 
+/** Ensures a character array conforms to the 'name' format
+ * All characters in a valid 'name' field should have a graphical representation 
+ * i.e. no control characters, whitespace, etc.
+ * A name field must also not be empty/NULL, and be null terminated, 
+ * and not eclipse DEFAULT_BUFFER_SIZE bytes.
+ *
+ * \param str 'name' string to be validated
+ * \return 1 if valid, 0 otherwise
+ */
 int isValidName(const char *str) {
   
   int validName = 0;
@@ -160,7 +185,7 @@ int isValidName(const char *str) {
 
   for (int index = 0; index < DEFAULT_BUFFER_SIZE; index++){
     
-
+    //Should always be 1, unless the first character is '\0'
     if (str[index] == '\0'){
       return validName;
     }
@@ -175,25 +200,34 @@ int isValidName(const char *str) {
   return 0;
 }
 
+
+/** Ensures a given character array is a valid 'Multiword'. 
+ * A valid multiword should only contain characters with graphical
+ * representations, and space characters.
+ * However, the first and last characters should not be space characters
+ * A multiword field must also not be empty/NULL, and be null terminated, 
+ * and not eclipse DEFAULT_BUFFER_SIZE bytes.
+ *
+ * \param str 'multiword' string to be validated.
+ * \return 1 if validated, 0 if invalid.
+ */
 int isValidMultiword(const char *str) {
+
   if(str == NULL){
     return 0;
   }
-  fflush(stdout);
-  //Check first letter not space
 
+  //Check the first character is not space
   if (!isgraph(str[0])){
     return 0;
   } 
-  fflush(stdout);
 
   int validMulti = 0;
-  fflush(stdout);
+
   for (int index = 1; index < DEFAULT_BUFFER_SIZE; index++){
     
-    //if string over, break the loop
+    //if end, check the last character (the previous one) and return
     if (str[index] == '\0'){
-      //check last letter not blank
       if (isspace(str[index - 1])){
         return 0;
       } else {
@@ -208,10 +242,22 @@ int isValidMultiword(const char *str) {
     }
 
   }
+
   return 0;
+
 }
 
+
+/** Ensures a given itemDetails struct is of the correct format
+ *  To be in the correct format, the 'name' field should be 
+ * a valid Name, and the 'desc' field should be a valid
+ * Multiword
+ *
+ * \param id ItemDetails struct to be validated
+ * \return 1 if validated, 0 if invalid.
+ */
 int isValidItemDetails(const struct ItemDetails *id) {
+
   if (id == NULL){
     return 0;
   }
@@ -227,6 +273,17 @@ int isValidItemDetails(const struct ItemDetails *id) {
   return 1; 
 
 }
+
+/** Ensures a given Character struct is of the correct format
+ * To be in the correct format, the 'name' field should be 
+ * a valid Multiword, the 'profession' field should be a valid
+ * name, the value of 'inventorySize' should not eclipse MAX_ITEMS,
+ * and the total items carried by the character should not eclipse 
+ * MAX_ITEMS.
+ *
+ * \param c Character struct to be validated
+ * \return 1 if validated, 0 if invalid.
+ */
 
 int isValidCharacter(const struct Character * c) {
   if (c == NULL){
@@ -260,6 +317,15 @@ int isValidCharacter(const struct Character * c) {
   return 1;
 }
 
+/** Validates and serialises a given array of Character structs to a file specified by the caller. 
+* The written file will contain a header, with the number of Character struct serialised,
+* followed by one or more blocks, containing information about each struct.
+* The function will return 0 upon success, and 1 otherwise.
+ * \param arr Array of Character structs
+ * \param nmemb Number of member Character structs in array 'arr'
+ * \param fd File Descriptor of file to be serialised to
+ * \return 0 if succesfully serialised, 1 if not succesfully serialised
+ */
 int saveCharacters(struct Character *arr, size_t nmemb, int fd) {
 
   if(fd<=2){
@@ -267,100 +333,105 @@ int saveCharacters(struct Character *arr, size_t nmemb, int fd) {
     return 1;
   }
 
-  //This might not be necessary
   int fdDup = dup(fd);
-  //check dup success
+
   if(fdDup<=2){
     printf("File fdDup error\n");
     return 1;
   }
 
-  printf("TEST 1\n");
-    fflush(stdout);
-
   FILE* fp = fdopen(fdDup, "w");
+  if(!fp){
+    printf("Error opening FP");
+    return 1;
+  }
   fflush(fp);
-
-  printf("TEST 1\n");
-    fflush(stdout);
 
   //Header
   uint64_t numItems = nmemb;
-  printf("TEST a\n");
-  fflush(stdout);
+
   if(fwrite(&numItems, sizeof(uint64_t), 1, fp) <= 0){
     printf("ERROR writing header");
     fflush(stdout);
     fflush(fp);
     fclose(fp);
-    
     return 1;
-
   }
 
   //Block
-  printf("TEST 222\n");
-    fflush(stdout);
   for (size_t index = 0; index < nmemb; index++){
-    printf("TEST 2\n");
-    fflush(stdout);
+    
     if (!isValidCharacter(&arr[index])){
       printf("INVALID CHARACTER\n\n");
       fflush(fp);
       fclose(fp);
       return 1;
     }
+
     if(fwrite(&arr[index], sizeof(struct Character), 1, fp) <=0){
       printf("ERROR writing body");
       fflush(fp);
       fclose(fp);
       return 1;
     }
+
   }
 
-  fflush(fp);
-  fclose(fp);
+  if (fclose(fp) != 0){
+    printf("File closing error");
+    return 1;
+  }
   return 0;
 }
 
-int loadCharacters(struct Character** ptr, size_t* nmemb, int fd) {
+/** If succesful, this function will deserialise details of items from a file into a pointer 
+* array of Character structs. The header of the file contains the number of Character 
+* elements to be deserialised, and is written into 'nmemb'
+* This number of Character elements is deserialised from the file specified by the inputted 
+* file descriptor, each validated and written into 'ptr'.
+* The memory of ptr is dynamically allocated, and should be freed by the caller
+* The function returns 0 on succesful deserialisation, and 1 otherwise
+ *
+ * \param ptr Pointer to Character struct pointer array to be deserialised into
+ * \param numItems Pointer to variable to have the number of Character elements written into
+ * \param fd File Descriptor to deserialised from
+ * \return 0 if successfully deserialised, 1 otherwise
+ */
 
+int loadCharacters(struct Character** ptr, size_t* nmemb, int fd) {
 
   if(fd<=2){
     printf("File fd error%i\n", fd);
     return 1;
   }
 
-  //This might not be necessary
   int fdDup = dup(fd);
-  //check dup success
+
   if(fdDup<=2){
     printf("File fdDup error\n");
     return 1;
   }
 
   FILE* fp = fdopen(fdDup, "r");
+  if(!fp){
+    printf("Error opening FP");
+    return 1;
+  }
   fflush(fp);
 
-  //uint64_t* headerBuf = (uint64_t*)malloc(sizeof(uint64_t));
+  //Header
   uint64_t headerBuf;
   
-  printf("FD %i", fd);
   if(fread(&headerBuf, sizeof(uint64_t), 1, fp) <= 0){
     printf("ERROR reading header");
     fflush(fp);
     fclose(fp);
-    //free(headerBuf);
     return 1;
-    //FLESH OUT
   }
-  
 
   *nmemb = headerBuf;
 
-  //size_t numElValue = (size_t) atoi(headerBuf);
-  //*numEls = numElValue;
-
+  //Block
   struct Character *structArrayBuffer = calloc(*nmemb, sizeof(struct Character));
   
   for (size_t i = 0; i < *nmemb; i++){
@@ -372,15 +443,11 @@ int loadCharacters(struct Character** ptr, size_t* nmemb, int fd) {
       free(structArrayBuffer);
       fflush(fp);
       fclose(fp);
-      //free(headerBuf);
-      //DO you only need to free pointers? what about numElValue and structBuffer?
       return 1;
-      //FLESH OUT
     }
 
     if(!isValidCharacter(&structBuffer)){
       printf("Invalid Character found in file");
-      free(nmemb);
       free(structArrayBuffer);
       fflush(fp);
       fclose(fp);
@@ -392,175 +459,108 @@ int loadCharacters(struct Character** ptr, size_t* nmemb, int fd) {
   }
 
   *ptr = structArrayBuffer;
-  fflush(fp);
-  fclose(fp);
+  
+  if (fclose(fp) != 0){
+    printf("File closing error");
+    return 1;
+  }
   return 0;
 }
 
 void playGame(struct ItemDetails* ptr, size_t nmemb){
+
   return;
 }
 
+/** 
+ * This function checks that the executable is run by 'pitchpoladmin'
+ * After validating the user, the eUID is set to the UID
+ * of pitchpoltadmin), an ItemDetails pointer array, and the number of elements 
+ * is deserialised from a file at the specified path. Following this, 
+ * permissions are dropped and the playGame function is called with the 
+ * deserialised data as parameters
+ * 
+ * \param filepath Path to file containing ItemDetails data
+ * \return 0 if all operations succesful, 1 if an error occurs with serialisation, 
+ * 2 if an error occurs with permissions.
+ */
+
 int secureLoad(const char *filepath) {
-  // Check guid as pitchpoladmin
   
-  struct passwd *ppAdmin;
-  ppAdmin = getpwnam("pitchpoladmin");
-  printf("TEST 1\n");
-    fflush(stdout);
+  struct passwd ppAdmin;
+  struct passwd *result;
+
+  long int bufferSize = sysconf(_SC_GETPW_R_SIZE_MAX);
+  if (bufferSize < 0){        
+    bufferSize = 16384;  
+  }
+
+  char* buf = malloc((size_t) bufferSize);
+  if (buf == NULL) {
+    printf("Malloc unsuccesful");
+    return 1;
+  }
+
+  if(getpwnam_r("pitchpoladmin", &ppAdmin, buf, (size_t) bufferSize, &result) != 0){
+    return 2;
+  }
+
+  if (result == NULL){
+    return 2;
+  }
 
   uid_t callerUid = getuid();
-  printf("TEST 1\n");
-    fflush(stdout);
 
-  if (callerUid != ppAdmin->pw_uid){
+  if (callerUid != ppAdmin.pw_uid){
     printf("Not the admin. Error");
     return 2;
   }
-  printf("TEST 1\n");
-    fflush(stdout);
 
-  if(seteuid(ppAdmin->pw_uid) == -1){
+  if(seteuid(ppAdmin.pw_uid) == -1){
     printf("Error with seteuid\n");
     return 2;
   }
-  printf("TEST 1\n");
-    fflush(stdout);
 
-  // check filetype, and ensure it is not malicious.  
   int fd = open(filepath, O_RDONLY);
 
   if (fd <= 2){
     printf("BAD FD");
     return 1;
   }
+
   struct stat fileStats;
+
   if(fstat(fd, &fileStats) == -1){
     printf("Error obtaining file stats\n");
     return 1;
   }
-  printf("TEST 1\n");
-    fflush(stdout);
 
-  if(!((fileStats.st_uid == ppAdmin->pw_uid) & (fileStats.st_gid == ppAdmin->pw_gid))){
+  if(!((fileStats.st_uid == ppAdmin.pw_uid) && (fileStats.st_gid == ppAdmin.pw_gid))){
     printf("Invalid file perms");
     return 1;
   }
 
-  // file is correct, and hasn't been swapped underneath us.
   struct ItemDetails *ptr;
   size_t nmemb;
+
   if(loadItemDetails(&ptr, &nmemb, fd) == 1){
     printf("Error with load item");
     return 1;
   }
 
-  close(fd);
+  if(close(fd) != 0){
+    printf("error closing fd");
+    return 1;
+  }
 
-  // drop privileges
   if(seteuid(callerUid) == -1){
     printf("error dropping");
     return 2;
   }
 
   playGame(ptr, nmemb);
+
+  free(buf);
   free(ptr);
   return 0;
 }
-
-
-
-
-/*
-int main(int argc,char *argv[]){
-    
-    // use appropriate location if you are using MacOS or Linux
-
-    const struct ItemDetails testItem1 = {
-      .itemID = 0,
-      .name = "testitem1",
-      .desc = "description 1",
-    } ;
-
-    const struct ItemDetails testItem2 = {
-      .itemID = 1,
-      .name = "testitem2",
-      .desc = " description of item 2",
-    } ;
-
-    const struct ItemDetails testItem3 = {
-      .itemID = 133,
-      .name = "test  item2",
-      .desc = "description of item 2",
-    } ;
-
-    
-
-    printf("Valid struct 1: %i\n", isValidItemDetails(&testItem1));
-    printf("Valid struct 2: %i\n", isValidItemDetails(&testItem2));
-    printf("Invalid struct 1: %i\n", isValidItemDetails(&testItem3));
-
-    struct ItemDetails * items = malloc(sizeof(struct ItemDetails) * 2);
-    items[0] = testItem1;
-    items[1] = testItem2;
-
-    int fd = open("/home/bob/Desktop/CITS3007/Project3007/test-code/test.txt",O_RDWR);
-    if(fd==-1){
-        printf("file not found.\n");
-        return -1;
-    }
-
-    saveItemDetails(items, 2, fd);
-    close(fd);
-
-    saveItemDetailsToPath(items, 2, "/home/bob/Desktop/CITS3007/Project3007/test-code/test2.txt");
-    
-    int fd2 = open("/home/bob/Desktop/CITS3007/Project3007/test-code/test.txt",O_RDONLY);
-    if(fd2<= 0){
-        printf("Invalid file \n");
-        return -1;
-    }
-    struct ItemDetails *ptr;
-    size_t* numEls = malloc(sizeof(size_t));
-    if (loadItemDetails(&ptr, numEls, fd2) == 0){
-      //Testing functionality of loadItemDetails
-      printf("Num els: %ld\n", *numEls);
-      printf("DESC 1:  %s\n", ptr[0].desc);
-      printf("NAME 1: %s\n", ptr[0].name);
-      printf("ITEMID 1: %li\n", ptr[0].itemID);
-      printf("DESC 2:  %s\n", ptr[1].desc);
-      printf("NAME 2: %s\n", ptr[1].name);
-      printf("ITEMID 2: %li\n", ptr[1].itemID);
-      close(fd2);
-
-      char* invalidName = "toktok EEE";
-      printf("INVALID name: %i\n", isValidName(invalidName));
-      char* validName = "toktokEEE";
-      printf("VALID name: %i\n", isValidName(validName));
-      fflush(stdout);
-
-      char *invalidMulti1 = NULL;
-      char *invalidMulti2 = " tr333";
-      char *invalidMulti3 = "qqq 3 ";
-      char *validMulti1 = "33   4 1";
-      char *validMulti2 = "2        wweru    ewfuwfr        uruwfr  1";
-      printf("INVALID multi 1: %i\n", isValidMultiword(invalidMulti1));
-      printf("INVALID multi 2: %i\n", isValidMultiword(invalidMulti2));
-      printf("INVALID multi 3: %i\n", isValidMultiword(invalidMulti3));
-      printf("VALID multi 1: %i\n", isValidMultiword(validMulti1));
-      printf("VALID multi 2: %i\n", isValidMultiword(validMulti2));
-
-
-      free(items);
-      printf("mickey");
-     
-
-    }
-    
-
-    
-    
-    
-    
-}
-*/
